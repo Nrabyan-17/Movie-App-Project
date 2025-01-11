c755a4244cf798a32cdaab9d736ddabbb9e4f296
Naufal Rizki Abyan <naufalrizkiabyan@gmail.com>
2025-01-11 23:19:01 +0700

    merubah data api

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,14 +1,14 @@
-const VITE_API_KEY = import.meta.env.VITE_API_KEY;
-const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getMovies() {
-    const response = await fetch(`${VITE_BASE_URL}/movie/popular?api_key=${VITE_API_KEY}`) // ---> function fetch() digunakan untuk mengirim permintaan HTTP ke server.
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`) // ---> function fetch() digunakan untuk mengirim permintaan HTTP ke server.
     const data = await response.json()  // ---> function json() digunakan untuk mengubah data yang diterima dari server yaitu 'response' menjadi format JSON.
     return data.results; // ---> data.results adalah data yang akan diambil dari hasil akhir response.
 };
 
 export async function searchMovies(query) {
-    const response = await fetch(`${VITE_BASE_URL}/search/movie?api_key=${VITE_API_KEY}&query=${encodeURIComponent(query)}`); // --> query yang diinputkan oleh user pada komponen search bar akan di encode menggunakan function encodeURIComponent().
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`); // --> query yang diinputkan oleh user pada komponen search bar akan di encode menggunakan function encodeURIComponent().
     const data = await response.json();
     return data.results;
 };
